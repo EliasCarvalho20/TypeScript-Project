e1c4ace13088ebdc8574aa219581bd1109f78f57
EliasCarvalho20 <eliascarvalho01@hotmail.com>
2021-03-14 21:42:52 -0300

    appointments created

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,5 +1,11 @@
 import { Router } from 'express';
 
+import appointmentsRouter from './appointmentsRoutes';
+import usersRouter from './usersRoutes';
+
 const routes = Router();
+
+routes.use('/appointments', appointmentsRouter);
+routes.use('/users', usersRouter);
 
 export default routes;
