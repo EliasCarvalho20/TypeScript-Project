ba2a58c00ff455bfef5e588c9f1640b002803da3
EliasCarvalho20 <eliascarvalho01@hotmail.com>
2021-03-17 22:57:32 -0300

    fixing typescript error

--- backend/src/middlewares/errorHandler.ts
+++ backend/src/middlewares/errorHandler.ts
@@ -2,8 +2,8 @@ import { Request, Response, NextFunction } from 'express';
 
 import AppError from '../library/errors/AppError';
 
-export default (err: Error, req: Request, res: Response, _: NextFunction): Response => {
-  const { message, statusCode } = err || undefined;
+export default (err: Error | AppError, req: Request, res: Response, _: NextFunction): Response => {
+  const { message, statusCode } = (err as AppError) || undefined;
 
   if (err instanceof AppError) return res.sendStatus(statusCode).json(message);
 
